85f8f9e127845e8d43e4a44e5ae3fb2260475a84
xuhaifengxx <54792446+xuhaifengxx@users.noreply.github.com>
2019-09-03 10:30:04 +0800

    Update test.c

--- test.c
+++ test.c
@@ -6,6 +6,7 @@ int main(int argc, const char *argv[])
 	printf("hello ubuntu");	
 	printf("hello C ");
 	printf("hello C++ ");
+	printf("hello C++ ");
 	printf("hello linux");
 	printf("hello");
 	return 0;
