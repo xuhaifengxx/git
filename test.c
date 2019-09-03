fd9b61d32161882127c81aa87bd4269107d3eaf7
xuhaifengxx <1126428575@qq.com>
2019-09-03 10:15:04 +0800

    modified test.c

--- test.c
+++ test.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-int int main(int argc, const char *argv[])
+int main(int argc, const char *argv[])
 {
 	printf("hello world");
-	
+	printf("hello ubuntu");	
 	printf("hello C ");
 	printf("hello C++ ");
 	printf("hello linux");
